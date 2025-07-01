808bfb1989ed776fcdfa90e3d54cc30cc54d65b5
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-07-01 10:47:03 +0530

    java

---
+++ Basic/practice.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class practice{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        //sub
+        int sub = a-b;
+        System.out.println("sub = "+sub);
+        //product
+        int product = a*b;
+        System.out.println("product = "+product);
+    }
+}
