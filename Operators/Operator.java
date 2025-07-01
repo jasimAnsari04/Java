b72404c3f8baa6e2ec9245261009c4762709d9a7
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-07-01 11:49:28 +0530

    Java

---
+++ Operators/Operator.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Operator{
+    public static void main(String args[]){
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the value of A =");
+        // int A = sc.nextInt();
+        // System.out.print("Enter the value of B =");
+        // int B = sc.nextInt();
+
+        // // arithmetic operator
+        // int Add = A+B;
+        // System.out.println("Add = "+ Add);
+        // int Sub = A-B;
+        // System.out.println("Sub = "+ Sub);
+        // int Mul = A*B;
+        // System.out.println("Mul = "+ Mul);
+        // int Div = A/B;
+        // System.out.println("Div = "+ Div);
+        // int Rem = A % B;
+        // System.out.println("Rem = "+ Rem);
+        
+
+        // relational opertor
+        // System.out.println(A == B);
+        // System.out.println(A != B);
+        // System.out.println(A > B);
+        // System.out.println(A < B);
+        // System.out.println(A >= B);
+        // System.out.println(A <= B);
+
+
+
+        //logical operator
+        // System.out.println( (3>2) && (5>2) );
+        // System.out.println( (3>2) && (5<2) );
+
+        // System.out.println( (3>2) || (5>2) );
+        // System.out.println( (3>2) || (5<2) );
+        // System.out.println( (3<2) || (5>2) );
+        // System.out.println( (3<2) || (5<2) );
+
+        // System.out.println( !(3>2) );
+    }
+}
