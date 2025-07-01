15f22a747ec8193a297a275ca9175d5fb426712b
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-07-01 12:32:01 +0530

    java

---
+++ Conditional Statements/Condition.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class Condition{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the Age");
+        // int age = sc.nextInt();
+        // if (age>=18){
+        //     System.out.println("adult");
+        // }
+        // else {
+        //     System.out.println("not adult");
+        // }
+
+
+        // largest of two
+        // int A = sc.nextInt();
+        // int B = sc.nextInt();
+        // if (A >= B){
+        //     System.out.println("A is largest");
+        // } else{
+        //     System.out.println("B is largest");
+        // }
+
+
+
+        // odd even
+        // System.out.println("Enter the Number");
+        // int num = sc.nextInt();
+        // if (num %2 == 0){
+        //     System.out.println(num +"is even");
+        // } else {
+        //     System.out.println(num +"is odd");
+        // }
+
+
+        // largest among the three
+        // System.out.println("Enter the Number1");
+        // int num1 = sc.nextInt();
+        // System.out.println("Enter the Number2");
+        // int num2 = sc.nextInt();
+        // System.out.println("Enter the Number3");
+        // int num3 = sc.nextInt();
+        // if((num1 >= num2) && (num1 >= num3)){
+        //     System.out.println("num1 is largest");
+        // }else if (num2 >= num3){
+        //     System.out.println("num2 is largest");
+        // }else{
+        //     System.out.println("num3");
+        // }
+
+
+        // int income = sc.nextInt();
+        // int tax;
+        // if(income < 500000){
+        //     tax = 0;
+        // } else if(income >= 500000 && income < 1000000){
+        //     tax = (int)(income * 0.2);
+        // } else {
+        //     tax = (int) (income * 0.3);
+        // }
+        // System.out.println("your tax is "+tax);
+    }
+}
