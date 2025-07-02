1156a2edbca35875ddf25a0914942c8792e3eb6c
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-07-02 12:15:36 +0530

    java

---
+++ loops/Loop.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Loop{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // int count = 0;
+        // while(count < 10){
+        //     System.out.println("Hello World");
+        //     count++;
+        // }
+
+        // int count = 1;
+        // while(count <= 10){
+        //     System.out.println(count);
+        //     count++;
+        // }
+
+
+        // System.out.print("Enter the range: ");
+        // int range = sc.nextInt();
+        // int count = 1;
+        // while(count <= range){
+        //     System.out.print(count + " ");
+        //     count++;
+        // }
+
+        // int count = 1;
+        // while(count <= 10){
+        //     System.out.println(count*2);
+        //     count++;
+        // }
+
+        // System.out.print("Enter the number: ");
+        // int number = sc.nextInt();
+        // int sum = 0;
+        // int i = 1;
+        // while(i <= number){
+        //     sum += i;
+        //     i++;
+        // }
+        // System.out.println(sum);
+    }
+}
