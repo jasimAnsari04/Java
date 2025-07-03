0af5567b8892ced8cdc0d1859ce509fab326e482
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-07-03 10:13:27 +0530

    java

--- loops/Loop.java
+++ loops/Loop.java
@@ -54,11 +54,72 @@ public class Loop{
         //     System.out.println("99 * " + i + " = " +i*99);
         // }
 
-        for(int lines = 1; lines <= 4; lines++){
-            System.out.println("* * * *");
+        // for(int lines = 1; lines <= 4; lines++){
+        //     System.out.println("* * * *");
+        // }
+
+        // reverse of a number
+        // int number = 987547;
+        // while(number>0){
+        //     int lastDigit = number%10;
+        //     System.out.print(lastDigit + "");
+        //     number = number/10;
+        // }
+        // System.out.println();
+
+
+        // int n =  10899;
+        // int rev = 0;
+        // while(n>0){
+        //     int lastDigit = n%10;
+        //     rev = (rev*10)+lastDigit;
+        //     n=n/10;
+        // }
+        // System.out.println(rev);
+
+
+        // int count = 1;
+        // do {
+        //     System.out.println("Hello World");
+        //     count++;
+        // }while(count <= 10);
+
+        //keep entering numbers till user enters a multiple of 10
+        // Scanner sc = new Scanner(System.in);
+        // do {
+        //     System.out.print("Enter your number: ");
+        //     int n = sc.nextInt();
+        //     if(n%10==0){
+        //         break;
+        //     }
+        //     System.out.println(n);
+        // }while(true);
+
+
+        // for(int i=1;i<=10;i++){
+        //     if(i==5){
+        //         continue;
+        //     }
+        //     System.out.println("2 * "+i+"="+2*i);
+        // }
+
+
+
+        //check number is prime or not
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        boolean isPrime = true;
+        for(int i=2;i<=n-1;i++){
+            if(n%i==0){
+                isPrime = false;
+            }
         }
-
-
+        if(isPrime==true){
+            System.out.println(n+" is prime");
+        }
+        else{
+            System.out.println(n+" is not prime");
+        }
 
     }
 }
