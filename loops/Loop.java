6e9851db76f7e837c0b85d5ea8cce2a3f60e4f3d
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-07-03 10:25:36 +0530

    java

--- loops/Loop.java
+++ loops/Loop.java
@@ -121,20 +121,23 @@ public class Loop{
         //     System.out.println(n+" is not prime");
         // }
 
-        System.out.print("Enter the number: ");
-        int n = sc.nextInt();
-        boolean isPrime = true;
-        for(int i=2;i<=Math.sqrt(n);i++){
-            if(n%i==0){
-                isPrime = false;
-            }
-        }
-        if(isPrime==true){
-            System.out.println(n+" is prime");
-        }
-        else{
-            System.out.println(n+" is not prime");
-        }
+        // System.out.print("Enter the number: ");
+        // int n = sc.nextInt();
+        // boolean isPrime = true;
+        // for(int i=2;i<=Math.sqrt(n);i++){
+        //     if(n%i==0){
+        //         isPrime = false;
+        //     }
+        // }
+        // if(isPrime==true){
+        //     System.out.println(n+" is prime");
+        // }
+        // else{
+        //     System.out.println(n+" is not prime");
+        // }
+
+
+        
 
     }
 }
